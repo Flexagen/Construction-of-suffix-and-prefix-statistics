2aac52312d9198ef2d179b7f85a4296c3bf2940f
KIvan1 <kirillov20403@gmail.com>
2023-05-29 18:22:11 +0300

    add classes

--- cpp_code/statistic_counter.cpp
+++ cpp_code/statistic_counter.cpp
@@ -3,7 +3,22 @@
 
 using namespace std;
 
+class node{
+public:
+    node* prev;
+    node* next[27];
+    int count;
+    node() {
+        count = 0;
+        prev = nullptr;
+    }
+};
+
+class statistic_counter{
+
+};
+
 int main()
 {
-    
+
 }
