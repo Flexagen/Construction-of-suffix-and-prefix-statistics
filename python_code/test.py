1984ea229464f8ba135fe9bdf5c74abff0c6ccff
Flexagen <maksim_pavlov_2003@list.ru>
2023-06-01 17:04:45 +0300

    upd

--- python_code/test.py
+++ python_code/test.py
@@ -1,5 +1,6 @@
 import StatistiCuM
 from prefix_statistics import Prefix_Stat
+from suffix_statistics import Suffix_Stat
 
 def test_statistic_class():
 	"""Тестирование .so обёртки для C++ структуры данных""" 
@@ -15,9 +16,9 @@ def test_statistic_class():
 	while(s != ''):
 		assert(s != '')
 		assert(s != None)
-		print(s)
+		# print(s)
 		s = p.get_next()
-	
+
 	if p.get_by_number(2) != "is the link":
 		raise AssertionError("Тест поиска префикса/суффикса по номеру")
 
@@ -25,13 +26,13 @@ def test_statistic_class():
 		raise AssertionError("Тест поиска префикса/суффикса")
 
 
-def test_suffix_statistic():
-	"""Тестирование модуля подсчёта статистики префиксов"""
-	p = Suffix_Stat('The is the link to the python package', 2)
-
-
-def test_suffix_statistic():
+def test_prefix_statistic():
 	None
+
+
+def test_suffix_statistic():
+	"""Тестирование модуля подсчёта статистики суффиксов"""
+	p = Suffix_Stat('The is the link to the python package', 2)
 
 
 def print_test_passed(test_name):
@@ -42,6 +43,12 @@ if __name__ == "__main__":
 	test_name = ""
 	try:
 		test_name = "statistic_class"; test_statistic_class(); print_test_passed(test_name)
+<<<<<<< HEAD
+=======
+		test_name = "prefix_statistic"; test_prefix_statistic(); print_test_passed(test_name)
+		test_name = "suffix_statistic"; test_suffix_statistic(); print_test_passed(test_name)
+		print("Well done!")
+>>>>>>> b26dea1 (upd)
 	except AssertionError as error:
 		print("Test "+test_name+' '+'\033[101m'+"ERROR"+'\033[0m')
 		print(error)
