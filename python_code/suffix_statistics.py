2ea52cfb16bda45a0da6391173e24a97837a217a
KIvan1 <kirillov20403@gmail.com>
2023-06-02 16:29:56 +0300

    Commit

--- python_code/suffix_statistics.py
+++ python_code/suffix_statistics.py
@@ -24,30 +24,10 @@ class SuffixStat:
         if (n < 1):
             return array
         s = self.stat.get_next()
-        n = n + 1
-        k = 0
-        count = 0
-        # print(n)
-        while(s != ''):
-            if(n > 0):
-                n -= 1
-                array.append(s.split(' '))
-                # arr.append([])
-                arr.append([s.split(' ')[0]])
-                # print (array)
-                # print (n)
-                if(len(array) > 1 and array[len(array)-2][1] == array[len(array)-1][1]):
-                    n += 1
-                count += 1
-            if k == 1:
-                array = array[:-1]
-                arr = arr[:-1]
-                print (arr)
-                break
-            if n == 0:
-                k = 1
-            s = self.stat.get_next()
-        n = 0
+        prev = -1
+        while s:
+
+
         return(arr)
 
     def most_common_in_word(self):
