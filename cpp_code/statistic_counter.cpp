2940a9ad6bd815b3852e36edeef9cdff9c80cb03
KIvan1 <kirillov20403@gmail.com>
2023-06-02 20:13:01 +0300

    I'm cummit

--- cpp_code/statistic_counter.cpp
+++ cpp_code/statistic_counter.cpp
@@ -2,11 +2,11 @@
 #include<string>
 #include<iostream>
 #include<algorithm>
-// #include<pybind11/pybind11.h>
-// #include<pybind11/stl.h>
-// #include<pybind11/numpy.h>
+#include<pybind11/pybind11.h>
+#include<pybind11/stl.h>
+#include<pybind11/numpy.h>
 
-// namespace py = pybind11;
+namespace py = pybind11;
 
 //Ето вам не надо
 class node{
